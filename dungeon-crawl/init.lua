a117c6d02d6098aba7b22cc201555c2cd5a266ed
NewbProgrammer101 <countolaf17@gmail.com>
2017-07-14 11:16:32 -0500

    Update init.lua

--- dungeon-crawl/init.lua
+++ dungeon-crawl/init.lua
@@ -1,4 +1,7 @@
 -- Gravestones, Mobs, and lore found in default dungeons.
+local path = minetest.get_modpath("dungeon-crawl")
+dungeon-crawl = {}
+
 minetest.register_node(":dungeon-crawl:dudresser", {
     description = "Dungeon drawer",
     tiles = {"dresser_top.png", "dresser_front.png"},
