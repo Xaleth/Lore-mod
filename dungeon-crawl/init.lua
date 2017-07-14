e2af2972c914ce5acc4d569cb4310c9194aa0031
NewbProgrammer101 <countolaf17@gmail.com>
2017-07-14 11:23:50 -0500

    Update init.lua

--- dungeon-crawl/init.lua
+++ dungeon-crawl/init.lua
@@ -2,6 +2,8 @@
 local path = minetest.get_modpath("dungeon-crawl")
 dungeon-crawl = {}
 
+dofile(path.."/spawn.lua")
+
 minetest.register_node(":dungeon-crawl:dudresser", {
     description = "Dungeon drawer",
     tiles = {"dresser_top.png", "dresser_front.png"},
